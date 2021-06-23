af53820213007d304fdd1020c2b40f93a064f930
gatesakagi <gatesakagi@gmail.com>
2021-06-24 01:31:40 +0800

    一般寫法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'dct110008-stopwatch-starter';
+export class AppComponent implements OnInit {
+
+  second = 0.0;
+  totalLapTimes: Array<number> = [];
+  oddLapTimes: Array<number> = [];
+  evenLapTimes: Array<number> = [];
+  intervalTimer: any;
+  isStartTimer = false;
+  clickStartDisabled = false;
+  clickPauseDisabled = true;
+  clickStopDisabled = true;
+  clickDivideDisabled = true;
+
+  ngOnInit() {
+
+  }
+
+  start() {
+    if(this.isStartTimer === false){
+      this.intervalTimer = setInterval(() => {
+        this.second += 0.1;
+      }, 100);
+      this.clickStartDisabled = true;
+      this.clickPauseDisabled = false;
+      this.clickStopDisabled = false;
+      this.clickDivideDisabled = false;
+      this.isStartTimer = true;
+
+    }
+    console.log('start');
+  }
+
+  pause() {
+    clearTimeout(this.intervalTimer);
+    this.isStartTimer = false;
+    this.clickPauseDisabled = true;
+    this.clickStartDisabled = false;
+    this.clickStopDisabled = false;
+    this.clickDivideDisabled = true;
+  }
+
+  stop() {
+    clearTimeout(this.intervalTimer);
+    this.second = 0;
+    this.isStartTimer = false;
+    this.clickPauseDisabled = true;
+    this.clickStartDisabled = false;
+    this.clickStopDisabled = true;
+    this.clickDivideDisabled = true;
+  }
+
+  divide() {
+    this.totalLapTimes.push(this.second);
+    this.oddLapTimes = this.totalLapTimes.filter((value, index) => index % 2 === 0);
+    this.evenLapTimes = this.totalLapTimes.filter((value, index) => index % 2 === 1);
+  }
 }
